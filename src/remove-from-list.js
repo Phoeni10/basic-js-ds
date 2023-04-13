adca69a05085c0c9f20daf038eee433d1b253061
Phoeni10 <webjscreator@gmail.com>
2023-04-13 19:05:30 +0300

    feat: solve remove from list (Thu 13.04.2023 19:05:35)

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -22,9 +22,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  if(l.value === k) {
+    l = l.next
+  };
+  let head = l;
+  
+
+  while(head.next) {
+    if(head.next.value === k) {
+      head.next = head.next.next
+    }
+    else {head = head.next};
+  }
+
+  return l;
 }
 
 module.exports = {
